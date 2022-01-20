d25cdecb5260c07f931f6b2532437a86dafa58d8
sgiraldo <sgiraldo@gearplug.io>
2022-01-20 09:11:03 -0500

    feat: setup file and initial package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="tiktok-marketing-python",
+    version="1.0.0",
+    description="Python developed library for TikTok Marketing API",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GearPlug/tiktok-marketing-python",
+    author="Santiago G. Alegria",
+    author_email="sgiraldo@gearplug.io",
+    license="MIT",
+    packages=["tiktok_marketing"],
+    install_requires=["requests"],
+    zip_safe=False,
+)
